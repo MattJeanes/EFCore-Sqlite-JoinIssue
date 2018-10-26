63f85551e612dd0fc9db283bae1ad6b5de070ebf
Matt Jeanes <matthew.jeanes@moneybarn.com>
2018-10-26 14:22:46 +0100

    Add project files.

---
+++ EFCore-Sqlite-JoinIssue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore_Sqlite_JoinIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
